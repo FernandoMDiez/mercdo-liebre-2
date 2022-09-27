a0510c234b3b490716502b9956da6b5c4529d1a5
Fernando Diez <fmd.diez10@gmail.com>
2022-09-27 19:38:26 -0300

    hasta la practica integrado - 07

--- app.js
+++ app.js
@@ -4,10 +4,25 @@ const path = require("path");
 const app = express();
 app.use(express.static("public"));
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.port || 3000, () => {
   console.log("Servidor levantado");
 });
 
 app.get("/", (req, res) => {
   res.sendFile(path.resolve("./views/home.html"));
+});
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.resolve("./views/login.html"));
+});
+
+app.post("/login", (req, res) => {
+  res.redirect("/");
+});
+
+app.get("/register", (req, res) => {
+  res.sendFile(__dirname + "/views/register.html");
+});
+app.post("/register", (req, res) => {
+  res.redirect("/");
 });
